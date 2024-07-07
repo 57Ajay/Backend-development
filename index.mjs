a6d24b5703eea4efa8b7aa335cb88896d2fd0233
57Ajay <57ajay.u@gmail.com>
2024-07-07 19:40:40 +0530

    creating a log Event module

--- index.mjs
+++ index.mjs
@@ -1,3 +1,16 @@
-import os from 'os';
+import logEvent from "./logEvent.mjs";
 
-console.log(os.homedir());
+import EventEmitter from 'events';
+
+// initialize a object; 
+class MyEmitter extends EventEmitter{};
+ 
+const myEmitter = new MyEmitter();
+
+//add a lister for a log event;
+
+myEmitter.on('log', (msg)=>logEvent(msg));
+
+setTimeout(()=>{
+    myEmitter.emit('log', 'log Event Emitted')
+}, 2000)
