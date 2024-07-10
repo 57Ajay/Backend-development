95b7153adb62c26cd55cff13cf5bcec7f0076c32
57Ajay <57ajay.u@gmail.com>
2024-07-10 17:00:22 +0530

    connecting MongoDB with mongoose

---
+++ backend/src/db/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`\n MongoDB connected, ${connectionInstance.connection.host}`)
+    }catch(error){
+        console.error("Error: ", error);
+        process.exit(1);
+    }
+};
+export default connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,42 +1,56 @@
-import express from 'express';
-import { config } from 'dotenv';
+
+import connectDB from "./db/index.js";
+import {config} from "dotenv";
 config();
 
-const app = express();
-const port = process.env.PORT;
 
-const jokes = [
-    {
-        setup: "Why don't scientists trust atoms?",
-        punchline: "Because they make up everything!"
-    },
-    {
-        setup: "Why did the scarecrow win an award?",
-        punchline: "Because he was outstanding in his field!"
-    },
-    {
-        setup: "Why don't they play poker in the jungle?",
-        punchline: "Too many cheetahs!"
-    }
-]
-
-app.get('/', (req, res) => {
-
-  res.send('Hello World! This is AJay');
-});
-
-app.get('/api/jokes', (req, res) => {
-    
-    res.send(jokes);
-});
-
-app.get('/api/jokes/random', (req, res) => {
-    const randomIndex = Math.floor(Math.random() * jokes.length);
-    const randomJoke = jokes[randomIndex];
-    res.send(randomJoke);
-});
+connectDB();
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+
+// (async ()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         console.log("Connected to DB");
+//         app.on("error", (error)=>{
+//             console.error("Error: ", error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`Server running on port ${process.env.PORT}`);
+//         });
+//     }catch(err){
+//         console.error("Error: ",err);
+//     }
+// })();
