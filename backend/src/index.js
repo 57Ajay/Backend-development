3c09a5f9dd11498dc4ba15e5539c31540bbda2d9
57Ajay <57ajay.u@gmail.com>
2024-07-10 17:57:39 +0530

    some important utils functions

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 import {config} from "dotenv";
 config();
 
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.error("Database connection Error: ", err);
+});
 
 
 
